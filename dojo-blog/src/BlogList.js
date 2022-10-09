79bc897a849a7e6ac734bcf105ff8349194038cd
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-10-09 18:37:34 +0000

    Learnt Using Props in ReactJS

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,19 +1,20 @@
-const BlogList = ({ blogs, title}) => {
+const BlogList = ({ blogs, title }) => {
+  // One more method to access the contents of props
   // const blogs = props.blogs;
   // const title = props.title;
   // console.log(blogs);
 
   return (
     <div className="blog-list">
-      <h2>{ title }</h2>
-      {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id} >
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
         </div>
       ))}
     </div>
   );
-}
- 
+};
+
 export default BlogList;
